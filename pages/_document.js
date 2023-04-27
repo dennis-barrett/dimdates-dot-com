12df8aeaa9aefafe2830541db22624454beb7a8a
Dennis Barrett <dbarrett6@gmail.com>
2023-04-27 17:53:06 +0200

    Misc changes, but mostly adding Google Analytics tag (#2)

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* <meta name="google-site-verification" content="1yE_GbND2ulcprJxgPNHSPaGCw3NMdvv0x1H2CiwvRY" /> */}
         <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet" />
+
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_TAG}`}
+          strategy="afterInteractive"
+        />
+        <Script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'process.env.GOOGLE_ANALYTICS_TAG');
+          `}
+        </Script>
       </Head>
 
       <body>
